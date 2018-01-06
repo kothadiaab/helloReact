dc69d4640609ef8948a2bd84efa4b55d3d6c87e6
kothadiaab <kothadiaab@gmail.com>
2018-01-05 20:16:36 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './container/App.jsx';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('site-wrapper')
+);
